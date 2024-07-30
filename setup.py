41937d53787d22e6da5f270155593a61795d97e5
Chuck Stearns <chuck.stearns@gmail.com>
2024-07-30 11:47:00 -0500

    nix-flake-testing

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 from time import strftime as date
 
 setup(name = "apyg",
-	version = "0.8b-" + date('%g%m%d-%H%M%S'),
-	description = "Random Password Generator.",
-	author = "Chuck Stearns",
-	author_email = "chuck.stearns@gmail.com",
-	url="",
-	packages = ['apyglib'],
-	scripts = ['apyg.py'],
-	)
+  version = "0.8b-" + date('%g%m%d-%H%M%S'),
+  description = "Random Password Generator.",
+  author = "Chuck Stearns",
+  author_email = "chuck.stearns@gmail.com",
+  url="",
+  packages = ['apyglib'],
+  scripts = ['apyg.py'],
+)
