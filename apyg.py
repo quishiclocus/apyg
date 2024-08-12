1e75cbd9b267bf83380348fa00aee07763ee8ecc
Chuck Stearns <chuck.stearns@gmail.com>
2024-08-12 11:15:37 -0500

    apyg

--- apyg.py
+++ apyg.py
@@ -1,44 +1,71 @@
 #!/usr/bin/env python
 
-'''
+"""
 quishiclocus, 2024
 v 0.9.0
 
 Generate random alphanumeric strings.
 See README for usage and installation.
-'''
+"""
 
 # Standard Library Imports
 import argparse
 import sys
-# Custom Imports
-from apyglib.RandomPasswordStack import *
-from apyglib.Phoenetic import *
-from apyglib.CryptPrint import *
+
+from apyglib.CryptPrint import CryptPrint
+from apyglib.Phoenetic import Phoenetic
+from apyglib.RandomPasswordStack import RandomPasswordStack
 
 # Set up default values and arguments with argparse
-o = argparse.ArgumentParser(description="A random password generator",
-                            epilog="Defaults: 1 password, 8 characters long")
+o = argparse.ArgumentParser(
+    description="A random password generator",
+    epilog="Defaults: 1 password, 8 characters long",
+)
 
-o.add_argument("-t", "--strict", action="store_true",
-               dest="strict", default=False,
-               help="require 1 each of upper, lower, digit, and special")
-o.add_argument("-p", "--phoenetic", action="store_true",
-               dest="phoenetic", default=False,
-               help="spell each password phoenetically")
-o.add_argument("-s", "--spec-chars", action="store_true",
-               dest="special_chars", default=False,
-               help="use special characters")
-o.add_argument("-y", "--crypt", action="store_true", dest="crypt",
-               help="print crypt hash of password")
-o.add_argument("-n", "--number", type=int, dest="n",
-               help="number of passwords to generate")
-o.add_argument("-l", "--maxlen", type=int, dest="l",
-               help="maximum length of passwords")
-o.add_argument("-m", "--minlen", type=int, dest="m", default=False,
-               help="if not defined, length is L")
-o.add_argument("-c", "--seed", dest="seed", default=False,
-               help="custom seed string")
+o.add_argument(
+    "-t",
+    "--strict",
+    action="store_true",
+    dest="strict",
+    default=False,
+    help="require 1 each of upper, lower, digit, and special",
+)
+o.add_argument(
+    "-p",
+    "--phoenetic",
+    action="store_true",
+    dest="phoenetic",
+    default=False,
+    help="spell each password phoenetically",
+)
+o.add_argument(
+    "-s",
+    "--spec-chars",
+    action="store_true",
+    dest="special_chars",
+    default=False,
+    help="use special characters",
+)
+o.add_argument(
+    "-y",
+    "--crypt",
+    action="store_true",
+    dest="crypt",
+    help="print crypt hash of password",
+)
+o.add_argument(
+    "-n", "--number", type=int, dest="n", help="number of passwords to generate"
+)
+o.add_argument("-l", "--maxlen", type=int, dest="l", help="maximum length of passwords")
+o.add_argument(
+    "-m",
+    "--minlen",
+    type=int,
+    dest="m",
+    default=False,
+    help="if not defined, length is L",
+)
+o.add_argument("-c", "--seed", dest="seed", default=False, help="custom seed string")
 
 o.set_defaults(n=1, l=8)
 
@@ -71,11 +98,11 @@ for x in range(opt.n):
 while len(g.pwords) > 0:
     newpword = g.pop()
     sys.stdout.write(newpword)
-# Strict checking
+    # Strict checking
     if g.strictpool(newpword, opt.strict) is True and opt.strict is True:
         # sys.stdout.write(" --> passes strict checks")
         sys.stdout.write("")
-# Alternate print options
+    # Alternate print options
     if opt.crypt:
         p = CryptPrint()
         p.printNewPasswordsCrypt(newpword, opt.l, opt.m)
