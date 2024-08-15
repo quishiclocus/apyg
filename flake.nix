3fa15288e70a8fbe72bfb1a059548c15aeaa1184
Chuck Stearns <chuck.stearns@gmail.com>
2024-08-15 17:45:36 -0500

    flake.nix appears to work - still need to test functionality

--- flake.nix
+++ flake.nix
@@ -17,20 +17,20 @@
           builder = with pkgs; "${bash}/bin/bash";
           args = [ "-c" "echo foo > $out" ];
         };
-        packages.shaka-packager =
+        packages.apyg =
           let
             version = "0.9.1";
             inherit (pkgs) stdenv lib;
           in
           stdenv.mkDerivation
             {
-              name = "shaka-packager-${version}";
+              name = "apyg-${version}";
 
               # https://nixos.wiki/wiki/Packaging/Binaries
               src = pkgs.fetchurl {
                 url =
                   "https://github.com/quishiclocus/apyg/releases/download/0.9.1-2/apyg-0.9.1.tar.gz";
-                sha256 = "sha256-4962ad1ce90a1af50ce4af1f486c4e02e0d297a883b90e317e6fce8cf399b21c";
+                sha256 = "sha256-r4BuwWFbQU5q0HVg82vELe/UyX9A788IOZkGDYBZpDc=";
               };
 
               sourceRoot = ".";
@@ -41,16 +41,16 @@
 
               buildPhase = ''
                 python -m build
-              ''
+              '';
 
               sourcePhase = ''
                 python -m build --sdist
-              ''
+              '';
+
               meta = with nixpkgs.lib; {
                 homepage = "https://github.com/quishiclocus/apyg/";
                 description =
                   "Generate random strings of varying length and complexity.";
-                platforms = platforms.linux;
               };
             };
       }
