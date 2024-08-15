31e8c1b9564e90f7f7940ae2d4ac3e571bff1fc4
Chuck Stearns <chuck.stearns@gmail.com>
2024-08-15 12:25:15 -0500

    work

--- flake.nix
+++ flake.nix
@@ -1,38 +1,52 @@
+# in flake.nix
 {
-  description = "Python flake for apyg Automatic Password Generator";
-
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      packages = forAllSystems ({ pkgs }: {
-        default =
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        name = "simple";
+        src = ./.;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = derivation {
+          inherit system name src;
+          builder = with pkgs; "${bash}/bin/bash";
+          args = [ "-c" "echo foo > $out" ];
+        };
+        packages.shaka-packager =
           let
-            python = pkgs.python312;
+            version = "0.9.1";
+            inherit (pkgs) stdenv lib;
           in
-          python.pkgs.buildPythonApplication {
-            name = "apyg";
+          stdenv.mkDerivation
+            {
+              name = "shaka-packager-${version}";
 
-            buildInputs = with python.pkgs; [ pip ];
+              # https://nixos.wiki/wiki/Packaging/Binaries
+              src = pkgs.fetchurl {
+                url =
+                  "https://github.com/shaka-project/shaka-packager/releases/download/v${version}/packager-linux-x64";
+                sha256 = "sha256-MoMX6PEtvPmloXJwRpnC2lHlT+tozsV4dmbCqweyyI0=";
+              };
 
-            src = ./.;
-          };
-      });
-    };
+              dontUnpack = true;
+              sourceRoot = ".";
+
+              installPhase = ''
+                install -m755 -D $src $out/bin/shaka-packager
+              '';
+
+              meta = with nixpkgs.lib; {
+                homepage = "https://shaka-project.github.io/shaka-packager/html/";
+                description =
+                  "Media packaging framework for VOD and Live DASH and HLS applications";
+                platforms = platforms.linux;
+              };
+            };
+      }
+    );
 }
